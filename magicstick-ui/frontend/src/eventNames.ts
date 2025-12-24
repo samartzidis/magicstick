806e86339dfcf61115850195a590650ae0231dd6
samartzidis <samartzidis@gmail.com>
2025-12-24 09:35:21 +0000

    -

--- magicstick-ui/frontend/src/eventNames.ts
+++
@@ -1,7 +0,0 @@
-// Event name constants - must match Go constants in main.go
-export const EventNames = {
-    BatteryReport: "battery-report",
-    DeviceDisconnected: "device-disconnected",
-    Rpc: "rpc-event",
-} as const;
-
